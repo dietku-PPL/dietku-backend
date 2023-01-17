447a51ad61e775ae04370d64c423430bcaaee24d
ibrohimhuzaimi <ibrohimhuzaimi@gmail.com>
2023-01-17 23:47:28 +0700

    add CORS

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+var cors = require('cors')
 const app = express()
 
 const allRoutes = require('./routes');
@@ -6,6 +7,7 @@ const allRoutes = require('./routes');
 const PORT = process.env.PORT || 3000
 
 app.use(express.json())
+app.use(cors())
 app.use(allRoutes)
 
 app.listen(PORT, () => {
